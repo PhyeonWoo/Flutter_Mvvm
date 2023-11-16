6b1f3f6be234e8e8f003ef5a9471f0373e203cbf
박현우 <simuoo96@gmail.com>
2023-11-16 19:28:57 +0900

    initial commit

---
+++ lib/normal/start.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:training/MVVM/views/mvvm_view.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Directionality(
+        textDirection: TextDirection.ltr, // 또는 rtl
+        child: PatternHome(),
+      ),
+    );
+  }
+}
+
+class PatternHome extends StatelessWidget {
+  const PatternHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('패턴')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('MVVM PATTERN'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MVVMView()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
